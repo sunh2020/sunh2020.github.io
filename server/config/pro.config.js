837a82234412a7f9b6002ce9bfa09cd28a74fa91
sunhenderson <39842892+sunh2020@users.noreply.github.com>
2021-04-08 13:44:00 -0700

    mysite

--- server/config/pro.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/prodb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-})
-    .then(()=>console.log("Database connection established"))
-    .catch(err=>console.log("There was an error", err))
